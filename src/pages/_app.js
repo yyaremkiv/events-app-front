d43bf1634f8ee17c2bc1e5a3071e46616866cd04
yyaremkiv <y.yaremkiv@gmail.com>
2023-05-12 22:39:52 +0300

    add auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,14 +1,20 @@
+import { SessionProvider } from "next-auth/react";
 import { Provider } from "react-redux";
 import { store } from "../redux/store.js";
 import { Layout } from "@/components/Layout/Layout";
 import "@/styles/globals.scss";
 
-export default function App({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <SessionProvider session={session}>
+      <Provider store={store}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </SessionProvider>
   );
 }
